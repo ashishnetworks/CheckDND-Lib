b6ad5c4e3d812eb5aff9e433790afd4b673db2e8
Ashish Networks <info2and@gmail.com>
2015-10-15 00:32:32 +0530

    Changed the name of main file to exmaple.py file and also deleted unsused files

---
+++ Example.py
@@ -0,0 +1,13 @@
+from CheckDnd import DND
+import sys
+
+sample=DND()
+v=sample.check('9876543210')
+if(v=='Y'):
+    print "DND Registered Number"
+elif(v=='N'):
+    print "DND Unregistered Number"
+elif(v==1):
+    print "DND Status Couldn't Get Confirmed"
+
+
